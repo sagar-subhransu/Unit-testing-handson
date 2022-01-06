c970a6b93466e75b308523c8a697192f392c4e7b
subhransu <subhransu.sharma@gmail.com>
2022-01-06 06:24:52 +0000

    unit testing basic details

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -5,4 +5,16 @@ import '../src/LoanBasicDetails/BasicDetails.js';
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+
+  it('Should render title ', async () => {
+    const el = await fixture(html`<basic-details><basic-details>`);
+    const h2 = el.shadowRoot?.querySelector('h2');
+    expect(h2).to.exist;
+  });
+  it('Should render LionInput', async () => {
+    const el = await fixture(html`<basic-details><basic-details>`);
+    const lionInput = await fixture(html`<lion-input></lion-input>`);
+    expect(lionInput).to.be.accessible();
+  });
+
 });
