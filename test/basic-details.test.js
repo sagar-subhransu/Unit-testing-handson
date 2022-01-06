925274b2056b29a72ca793f3d5c057a474380b35
Administrator <Administrator@EC2AMAZ-ACBI12F>
2022-01-06 09:26:35 +0000

    customer details

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -2,19 +2,24 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
+const el = await fixture(html`<basic-details><basic-details>`);
+
 describe('Basic details', () => {
   // Write test cases inside this block
   // refer basic-details.js files
+  it('check for accessibility' ,() => {
+    expect(el).to.be.accessible;
+  });
 
   it('Should render title ', async () => {
-    const el = await fixture(html`<basic-details><basic-details>`);
     const h2 = el.shadowRoot?.querySelector('h2');
     expect(h2).to.exist;
   });
+
   it('Should render LionInput', async () => {
-    const el = await fixture(html`<basic-details><basic-details>`);
     const lionInput = await fixture(html`<lion-input></lion-input>`);
-    expect(lionInput).to.be.accessible();
+    expect(lionInput).to.be.accessible;
   });
+
 
 });
--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -2,6 +2,15 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/Customer/Customer-details.js';
 
+const el = await fixture(html`<customer-details>/</customer-details>`);
+
 describe('customer details', () => {
   // Write test cases inside this block
+  it('check for accessibility' ,() => {
+    expect(el).to.be.accessible;
+  });
+  it('Should render title ', async () => {
+      const h2 = el.shadowRoot?.querySelector('h2');
+      expect(h2).to.exist;
+    });
 });
