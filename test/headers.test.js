14f5323ba7b0245f6faf4ae6d85d0f6466d79d62
sagar-subhransu <subhransu.sharma@gmail.com>
2022-01-13 10:03:07 +0530

    added test cases  for header and loan application

--- test/headers.test.js
+++ test/headers.test.js
@@ -4,8 +4,17 @@ import '../src/header/Header.js';
 
 describe('loan-header', () => {
   // Write test cases inside this block
-  it('check for accessibility' ,async () => {
+  it('check for accessibility' , () => {
     const el = await fixture(html`<loan-header></loan-header>`);
     expect(el).to.be.accessible;
+  });
+
+  it('calls localeChanged function when a button is clicked', async () => {
+    const el = await fixture(html`<loan-header></loan-header>`);
+    const myFunctionStub = stub(el, 'localeChanged');
+    el.requestUpdate();
+    await el.updateComplete;
+    el.shadowRoot.querySelector('button').click();
+    expect(myFunctionStub).to.have.callCount(1);
   });
 });
--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -4,8 +4,18 @@ import '../loan-application.js';
 
 describe('LoanApplication', () => {
   // Write test cases inside this block
-  it('check for accessibility' ,async () => {
+  it('check for accessibility' , () => {
     const el = await fixture(html`<loan-application></loan-application>`);
     expect(el).to.be.accessible;
+  });
+  it('Check title', () => {
+    const el = await fixture(html`<loan-application></loan-application>`);
+    expect(el.title).to.equal('Hey there');
+    expect(el.counter).to.equal(5);
+  });
+
+  it('Is custom element dashboard accessible', async () => {
+    const el = await fixture(html`<dash-board></dash-board>`);
+    await expect(el).to.be.accessible;
   });
 });
